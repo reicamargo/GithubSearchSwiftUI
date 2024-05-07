56d4e23b7e7f96b3bd84562543b9074e72b34d34
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-05-07 16:28:51 -0300

    Initial Commit

---
+++ GithubSearchSwiftUI/GithubSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchSwiftUIApp.swift
+//  GithubSearchSwiftUI
+//
+//  Created by Reinaldo Camargo on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
